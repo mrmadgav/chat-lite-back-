0282e15a89a05742d712c035b7450200d9a08648
madgav <madgav@mail.ru>
2021-08-06 23:44:08 +0600

    change origin

--- server.js
+++ server.js
@@ -14,12 +14,12 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const http = require("http").createServer(app);
-  
-const io = require("socket.io")(http, {  
+
+const io = require("socket.io")(http, {
   cors: {
-    origins: "*:*",
+    origins: "https://mrmadgav.github.io/chat-lite/",
     methods: ["GET", "POST"],
-    allowedHeaders: ["content-type"],
+    credentials: true,
   },
 });
 
