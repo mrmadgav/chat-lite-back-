1fb874f5b4492b897998b11c1ac6965578b3a1f5
madgav <madgav@mail.ru>
2021-08-08 02:17:26 +0600

    deleted const PORT

--- server.js
+++ server.js
@@ -25,12 +25,11 @@ require("dotenv").config();
 //   },
 // });
 
-const PORT = process.env.PORT || 3000;
 const INDEX = "/index.html";
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+  .listen(process.env.PORT || 3000, () => console.log(`Listening on ${PORT}`));
 
 const io = socketIO(server);
 
