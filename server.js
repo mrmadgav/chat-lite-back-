ce6bad04baefcbb1423e4e3b93fa7e30e364a447
madgav <madgav@mail.ru>
2021-08-08 01:15:30 +0600

    socket with empty properties

--- server.js
+++ server.js
@@ -15,15 +15,15 @@ require("dotenv").config();
 
 const http = require("http").createServer(app);
 
-// const io = require("socket.io")(http);
+const io = require("socket.io")(http);
 
-const io = require("socket.io")(http, {
-  cors: {
-    origin: "https://chat-lite-back.herokuapp.com/",
-    methods: ["GET", "POST"],
-    credentials: true,
-  },
-});
+// const io = require("socket.io")(http, {
+//   cors: {
+//     origin: "https://chat-lite-back.herokuapp.com/",
+//     methods: ["GET", "POST"],
+//     credentials: true,
+//   },
+// });
 
 // http.listen(process.env.PORT || 3000, function () {
 //   console.info("Server is running");
