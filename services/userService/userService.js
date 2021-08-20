557805e4f0121c92c0cc41ff30d95b67e25953f5
madgav <madgav@mail.ru>
2021-08-21 00:35:34 +0600

    add private emit evts

--- services/userService/userService.js
+++ services/userService/userService.js
@@ -185,7 +185,7 @@ const deleteMessage = async (id, roomId) => {
         );
     return !roomId
       ? io.emit("DeletingMessage")
-      : io.emit("privateMessage:fromServer", roomId);
+      : io.emit("privateDeleteMessage:fromServer", roomId);
   } catch (e) {
     console.error(e);
   }
@@ -205,7 +205,7 @@ const updateMessage = async (id, text, roomId) => {
         );
     return !roomId
       ? io.emit("User edit message")
-      : io.emit("privateMessage:fromServer", roomId);
+      : io.emit("privateEditMessage:fromServer", roomId);
   } catch (e) {
     console.error(e);
   }
